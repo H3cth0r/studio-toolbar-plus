4e33c7f84af8fcf83a7a8a25eecf59eed5b20de0
h3cth0r <hectorsucre13@gmail.com>
2025-10-16 23:54:39 -0600

    adding base structure and func

--- src/components/MagicLayoutsMapperModal.tsx
+++ src/components/MagicLayoutsMapperModal.tsx
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Modal } from "@mantine/core";
+import { 
+  Modal,
+  Box,
+  Title,
+} from "@mantine/core";
+import {
+  getAllLayouts,
+} from "../studio/layoutHandler";
+import { appStore } from "../modalStore";
 
 interface MagicLayoutsMapperModalProps {
   opened: boolean;
@@ -7,19 +15,63 @@ interface MagicLayoutsMapperModalProps {
 }
 
 export function MagicLayoutsMapperModal({ opened, onClose }: MagicLayoutsMapperModalProps) {
-  
+  const raiseError = appStore((store) => store.raiseError);
+
   const handleClose = () => {
     onClose();
   };
+
+  // Function that gets all Magic Layouts
+  const getMagicLayouts = async () => {
+    // Get all layouts
+    const layoutsResult = await getAllLayouts(window.SDK);
+    if (layoutsResult.isError()) {
+      raiseError(new Error("Failed to get layouts"));
+      throw new Error("Failed to get layouts");
+    }
+    const layouts = layoutsResult.value;
+
+    // Ensure layouts is defined
+    if (!layouts) {
+      raiseError(new Error("Layouts data is undefined"));
+      throw new Error("Layouts data is undefined");
+    }
+
+    // Find all layouts that start with ✨
+    const magicLayouts = layouts.filter((layout) =>
+      layout.name.startsWith("✨"),
+    );
+    console.log(magicLayouts);
+
+  };
+
+  useEffect(() => {
+    const executeLayoutDataFetch = async () => {
+      try {
+        await getMagicLayouts();
+      } catch (error) {
+        console.error("Magic process failed:", error);
+      }
+    };
+    executeLayoutDataFetch();
+  }, [opened]);
 
   return(
     <Modal
       opened={opened}
       onClose={handleClose}
-      title="Magic Layouts Mapper"
+      title="✨ Magic Layouts Mapper"
       size="xl"
-      fullscreen
+      fullScreen
     >
+      <Box
+        style={{
+          height: "calc(100vh - 120px)",
+          overflowY: "auto",
+          padding: "16px",
+        }}
+      >
+      </Box>
     </Modal>
   );
 }
